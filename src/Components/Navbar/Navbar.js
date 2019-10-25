5c1700f633118e49a8b61961f7509b1db91c1e93
johnnyjuarez <johnnyjuarez1992@gmail.com>
2019-10-24 22:48:31 -0500

    added react-bootstrap - and added navbar

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import './Navbar.css'
+
+class Navigation extends Component {
+    render() {
+        return (
+            <Navbar className="navbar" expand="lg">
+                <Navbar.Brand bsPrefix="navbar" className="brandItem" href="#home">The Knowledge Base</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link className="navItem" href="#home">Home</Nav.Link>
+                        <Nav.Link className="navItem" href="#link">About</Nav.Link>
+                        <Nav.Link className="navItem" href="#link">Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
+}
+
+export default Navigation
